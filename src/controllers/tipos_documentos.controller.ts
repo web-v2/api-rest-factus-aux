e74267d5cb9c0fbcd97e028f7ae945ee1b263357
Samir Vergara V <svergara3@misena.edu.co>
2025-01-29 19:11:43 -0500

    update

--- src/controllers/tipos_documentos.controller.ts
+++ src/controllers/tipos_documentos.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { getTiposDocumentos } from "../models/tipos_documentos.model";
+import { getFecha } from '../utils/fechas.utils'
 
 export const obtenerTiposDocumentos = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
+    console.log(getFecha(),'->Listar tipos de documentos');
     const tiposDocumentos = await getTiposDocumentos();
     res.status(200).json(tiposDocumentos);
   } catch (error) {

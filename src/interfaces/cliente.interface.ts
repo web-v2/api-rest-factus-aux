15cb3401b4350686b3308bb6bf30fbd1b3c30829
Samir Vergara V <svergara3@misena.edu.co>
2025-02-02 22:16:34 -0500

    update

--- src/interfaces/cliente.interface.ts
+++ src/interfaces/cliente.interface.ts
@@ -1,14 +1,14 @@
   export interface Cliente {
     identification: string;
-    dv?: number; // Digito de verificacion. se envia si es nit
+    dv?: number; 
     company?: string;
     trade_name?: string;
     names: string;
     address: string;
     email: string;
     phone: string;
-    legal_organization_id: number; //Tipo de organizacion, persona natural o juridica. se consume de tabla
-    tribute_id: number; // Si aplica o no aplica iva. se consume de tabla
-    identification_document_id: number; // Tipo de identificacion se consume de tabla
-    municipality_id: number; // municipio del cliente, se consume del endpoint municipios
+    legal_organization_id: number; 
+    tribute_id: number;
+    identification_document_id: number;
+    municipality_id: number;
   }
